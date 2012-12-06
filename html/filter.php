d340a67517f76b834614a08dc29b65bf4d43b96e
Kyle Krueger <kylekrueger@college.harvard.edu>
2012-12-06 03:23:05 -0500

    scope

--- html/filter.php
+++ html/filter.php
@@ -1,22 +1,28 @@
 <?php
     
+    // configuration
+    require("../includes/config.php");
+    
     if ($_POST["Year"] = 1)
         $n = $_POST["Group_Size"];
     else if ($_POST["Year"] = 2)
         $n = $_POST["Group_Size"] + 1;
     
     $rows = query("SELECT * FROM joining_suites WHERE n = ?", $n);
+    $counter = 0;
+    $table = [];
 
     foreach ($rows as $row)
     {
-        
+        $counter++;
+           
         if ($row["suite3"] = NULL && $row["suite2"] != NULL)
          {    
             $suite1 = $row["suite1"];
             
             $id1 = query("SELECT * FROM suites WHERE id = ?", $suite1);
             
-            $name1 = "$id1["entryway"]$id1["number"]";
+            $name1 = "{$id1["entryway"]}{$id1["number"]}";
             $name2 = NULL;
             $name3 = NULL;
             $name4 = NULL;
@@ -25,7 +31,7 @@
             
             $avg = $id1["averagebedroom"];
      
-            $table[] = [$n, $name1, $name2, $name3, $name4, $crm, $avg);
+           $table[$counter] = [$n, $name1, $name2, $name3, $name4, $crm, $avg];
         }
         
         if ($row["suite3"] = NULL && $row["suite2"] != NULL)
@@ -36,8 +42,8 @@
             $id1 = query("SELECT * FROM suites WHERE id = ?", $suite1);
             $id2 = query("SELECT * FROM suites WHERE id = ?", $suite2);
             
-            $name1 = "$id1["entryway"]$id1["number"]";
-            $name2 = "$id2["entryway"]$id2["number"]";
+            $name1 = "{$id1["entryway"]}{$id1["number"]}";
+            $name2 = "{$id2["entryway"]}{$id2["number"]}";
             $name3 = NULL;
             $name4 = NULL;
             
@@ -45,7 +51,7 @@
             
             $avg = ((($id1["averagebedroom"]*$id1["n"])+($id2["averagebedroom"]*$id2["n"]))/$n);
      
-            $table[] = [$n, $name1, $name2, $name3, $name4, $crm, $avg);
+            $table[$counter] = [$n, $name1, $name2, $name3, $name4, $crm, $avg];
         }
         
         if ($row["suite4"] = NULL && $row["suite3"] != NULL)
@@ -58,9 +64,9 @@
             $id2 = query("SELECT * FROM suites WHERE id = ?", $suite2);
             $id3 = query("SELECT * FROM suites WHERE id = ?", $suite3);
             
-            $name1 = "$id1["entryway"]$id1["number"]";
-            $name2 = "$id2["entryway"]$id2["number"]";
-            $name3 = "$id3["entryway"]$id3["number"]";
+            $name1 = "{$id1["entryway"]}{$id1["number"]}";
+            $name2 = "{$id2["entryway"]}{$id2["number"]}";
+            $name3 = "{$id3["entryway"]}{$id3["number"]}";
             $name4 = NULL;
             
             $crm = max($id1["commonroom"],$id2["commonroom"],$id3["commonroom"]);
@@ -68,7 +74,7 @@
             $avg = ((($id1["averagebedroom"]*$id1["n"])+($id2["averagebedroom"]*$id2["n"])+($id3["averagebedroom"]
               *$id3["n"]))/$n);
      
-            $table[] = [$n, $name1, $name2, $name3, $name4, $crm, $avg);
+           $table[$counter] = [$n, $name1, $name2, $name3, $name4, $crm, $avg];
         }
         
         if ($row["suite4"] != NULL)
@@ -92,11 +98,12 @@
             
             $avg = ((($id1["averagebedroom"]*$id1["n"])+($id2["averagebedroom"]*$id2["n"])+($id3["averagebedroom"]
               *$id3["n"])+($id4["averagebedroom"]*$id4["n"]))/$n);
-     
-            $table[] = [$n, $name1, $name2, $name3, $name4, $crm, $avg);
+              
+            $table[$counter] = [$n, $name1, $name2, $name3, $name4, $crm, $avg];
         }
+        
     }
     
-    render("filter_form.php", "table" => $table);
+    render("filter_form.php", ["table" => $table]);
     
 ?>
