471c8d9b43af631b4137eda5dc6872078f17dfb3
Kyle Krueger <kylekrueger@college.harvard.edu>
2012-12-08 13:57:06 -0500

    if conditions

--- html/favorite.php
+++ html/favorite.php
@@ -3,10 +3,12 @@
     // configuration
     require("../includes/config.php");
     
-    $insert = query("INSERT INTO favorites (suite_id, n, suite1, suite2, suite3, suite4, commonroom, averagebedroom) 
-           VALUES (?, ?, ?, ?, ?, ?, ?, ?) WHERE suite_id = ?", $table[7], $table[1], $table[2], $table[3], 
-           $table[4], $table[5], $table[6], $table[7]);
+    query("INSERT INTO favorites (user, suite_id, n, suite1, suite2, suite3, suite4, commonroom, averagebedroom) 
+           VALUES (?, ?, ?, ?, ?, ?, ?, ?)", $SESSION["id"], $table[7], 
+           $table[0], $table[1], $table[2], $table[3], $table[4], $table[5], $table[6]);
     $favorites = query("SELECT * FROM favorites");
     
-    render("filter_form.php", ["favorites" => $favorites]);
+    render("favorite_form.php", ["favorites" => $favorites]);
+    
+    redirect("/filter.php");
 ?>
--- templates/filter_form.php
+++ templates/filter_form.php
@@ -10,28 +10,28 @@
         <form name="filters">
            <strong>View All:</strong> 
            <input type="checkbox" id="all" checked="checked"><br/>
-           <strong>Floor:</strong> 
-           <input type="checkbox" id="floor1" checked="checked" value="1"> 1 
-           <input type="checkbox" id="floor2" checked="checked" value="2"> 2 
-           <input type="checkbox" id="floor3" checked="checked" value="3"> 3 
-           <input type="checkbox" id="floor4" checked="checked" value="4"> 4 
-           <input type="checkbox" id="floor5" checked="checked" value="5"> 5<br/>
+           <strong>Floor:</strong>
+           <input type="checkbox" id="floor1" onchange=printTable() checked="checked" value="1"> 1 
+           <input type="checkbox" id="floor2" onchange=printTable() checked="checked" value="2"> 2 
+           <input type="checkbox" id="floor3" onchange=printTable() checked="checked" value="3"> 3 
+           <input type="checkbox" id="floor4" onchange=printTable() checked="checked" value="4"> 4 
+           <input type="checkbox" id="floor5" onchange=printTable() checked="checked" value="5"> 5<br/>
            <strong>Entryway:</strong> 
-           <input type="checkbox" id="a" checked="checked" value="A"> A 
-           <input type="checkbox" id="b" checked="checked" value="B"> B 
-           <input type="checkbox" id="c" checked="checked" value="C"> C 
-           <input type="checkbox" id="d" checked="checked" value="D"> D 
-           <input type="checkbox" id="e" checked="checked" value="E"> E 
-           <input type="checkbox" id="f" checked="checked" value="F"> F 
-           <input type="checkbox" id="g" checked="checked" value="G"> G 
-           <input type="checkbox" id="h" checked="checked" value="H"> H 
-           <input type="checkbox" id="i" checked="checked" value="I"> I 
-           <input type="checkbox" id="j" checked="checked" value="J"> J 
-           <input type="checkbox" id="k" checked="checked" value="K"> K 
-           <input type="checkbox" id="l" checked="checked" value="L"> L 
-           <input type="checkbox" id="m" checked="checked" value="M"> M 
-           <input type="checkbox" id="n" checked="checked" value="N"> N 
-           <input type="checkbox" id="o" checked="checked" value="O"> O
+           <input type="checkbox" id="a" onchange=printTable() checked="checked" value="A"> A 
+           <input type="checkbox" id="b" onchange=printTable() checked="checked" value="B"> B 
+           <input type="checkbox" id="c" onchange=printTable() checked="checked" value="C"> C 
+           <input type="checkbox" id="d" onchange=printTable() checked="checked" value="D"> D 
+           <input type="checkbox" id="e" onchange=printTable() checked="checked" value="E"> E 
+           <input type="checkbox" id="f" onchange=printTable() checked="checked" value="F"> F 
+           <input type="checkbox" id="g" onchange=printTable() checked="checked" value="G"> G 
+           <input type="checkbox" id="h" onchange=printTable() checked="checked" value="H"> H 
+           <input type="checkbox" id="i" onchange=printTable() checked="checked" value="I"> I 
+           <input type="checkbox" id="j" onchange=printTable() checked="checked" value="J"> J 
+           <input type="checkbox" id="k" onchange=printTable() checked="checked" value="K"> K 
+           <input type="checkbox" id="l" onchange=printTable() checked="checked" value="L"> L 
+           <input type="checkbox" id="m" onchange=printTable() checked="checked" value="M"> M 
+           <input type="checkbox" id="n" onchange=printTable() checked="checked" value="N"> N 
+           <input type="checkbox" id="o" onchange=printTable() checked="checked" value="O"> O
            <br/>
         </form>
     </div>
@@ -68,7 +68,7 @@
                 {
                 foreach ($table as $t)
                     {
-                        print("<tr>");
+                        print("<tr ") print("class= 
                         print("<td>" . $t[0] . "</td>");      
                         print("<td>" . $t[1] . "</td>");
                         print("<td>" . $t[2] . "</td>");
@@ -94,6 +94,7 @@
         </form>   
 </div>
 <div id="theData"><?php print json_encode($table) ?></div>
+
 <script>
     // hide and get the data from the server
     $("#theData").hide();
@@ -112,15 +113,23 @@
             // check to make sure fits all the filters
             console.log(row);
             var entryway1 = row[1][0];
-            var entryway2 = row[2][0];
+            if (row[2][0] != NULL)
+                var entryway2 = row[2][0];
+            else
+                entryway2 = 0;
             var entryway3 = row[3][0];
             var entryway4 = row[4][0];
             var theFloor = row[1][1];
-            if (true){$("#floor1" + theFloor).is(':checked') || $("#floor2" + theFloor).is(':checked') 
-            && $("#floor3" + theFloor).is(':checked') || $("#floor4" + theFloor).is(':checked') || 
-            $("#floor5" + theFloor).is(':checked') || ($("#a" + entryway1).is(':checked') 
-            || $("#a" + entryway2).is(':checked') || $("#a" + entryway3).is(':checked') || $("#a" + 
-            entryway4).is('checked')) }
+            if (($("#floor1").checked() && $("#floor1").value() == theFloor) ||
+                ($("#floor2").checked() && $("#floor2").value() == theFloor) ||
+                ($("#floor3").checked() && $("#floor3").value() == theFloor) ||
+                ($("#floor4").checked() && $("#floor4").value() == theFloor) ||
+                ($("#floor5").checked() && $("#floor5").value() == theFloor) ||
+            /*|| $("#floor2" + theFloor).checked() 
+            || $("#floor3" + theFloor).checked() || $("#floor4" + theFloor).checked() || 
+            $("#floor5" + theFloor).checked() || ($("#a" + entryway1).checked() 
+            || $("#a" + entryway2).checked() || $("#a" + entryway3).checked() || $("#a" + 
+            entryway4).checked())*/)
             {
                 var thisRowHtml = "<tr><td>" + row[0] + "</td><td>" + row[1] + 
                     "</td><td>" + row[2] + "</td><td>" + row[3] + "</td><td>" + row[4] + 
@@ -130,13 +139,17 @@
                     + "class='btn'>" + "Add</button></td></tr>";
                 newTableHtml = newTableHtml + thisRowHtml;
             }
-        }
+        
         $(table).find("tbody").html(newTableHtml);      
     }
     
+    
     // click handler and initial print
-    $(function(){
+    $(function()
+    {
        $(".filter").click(function(event){printTable(event)});
        printTable();
     })
+    
+    }
 </script>
